eaa92138b5885c1d6519bde9395fe3901acaf53b
jobdk <47722294+jobdk@users.noreply.github.com>
2021-04-17 20:38:02 +0200

    made functions to arrow functions

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,4 +1,4 @@
-sap.ui.define(["sap/ui/core/UIComponent", "sap/ui/core/ComponentSupport"], function (UIComponent) {
+sap.ui.define(["sap/ui/core/UIComponent", "sap/ui/core/ComponentSupport"], (UIComponent) => {
     "use strict";
     return UIComponent.extend("de.blackforestsolutions.dravelopsconfigfrontend.Component", {
         metadata: {
--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -3,13 +3,13 @@ sap.ui.define(
         "sap/ui/Device",
         "sap/ui/core/mvc/Controller",
     ],
-    function (Device, Controller) {
+    (Device, Controller) => {
         "use strict";
 
         return Controller.extend(
             "de.blackforestsolutions.dravelopsconfigfrontend.controller.App",
             {
-                onInit: function () {
+                onInit: () => {
 
                 },
             }
