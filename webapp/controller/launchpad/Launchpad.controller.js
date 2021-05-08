38beafb19abd2895bd5d3fb4bb8588eb333b8fcf
jobdk <johnybudnik+git@gmail.com>
2021-05-08 10:27:14 +0200

    adjusted lint to support async and jquery added get Request to a mock up server backend

--- webapp/controller/launchpad/Launchpad.controller.js
+++ webapp/controller/launchpad/Launchpad.controller.js
@@ -1,46 +1,40 @@
 /**Launchpad is the main page of the application*/
 
 sap.ui.define(
-    [
-        "sap/ui/model/json/JSONModel",
-        "de/blackforestsolutions/dravelopsconfigfrontend/controller/BaseController"
+    ["de/blackforestsolutions/dravelopsconfigfrontend/controller/BaseController"
 
     ],
     function (BaseController
-        , JSONModel
     ) {
         "use strict";
         const apiSettingsTileId = "container-dravelopsconfigfrontend---launchpad--apiSettingsTile";
         const routeApiSettingsTile = "apisettings";
+        const errorMessage = "Routing to API Settings was not possible.";
+        var url = "http://localhost:8080/config_backend/apisettings";
         return BaseController.extend(
             "de.blackforestsolutions.dravelopsconfigfrontend.controller.launchpad.Launchpad",
             {
                 onInit: function () {
                 },
 
+                /*when tile is clicked on launchpad a request is sent to backend and user is routed to suitable view
+                 * which is filled by backend response
+                 * @param oEvent*/
                 tileClicked(oEvent) {
                     const pressedTileId = oEvent.getSource().getId();
-                    // const oRouter = this.getOwnerComponent().getRouter();
+                    const oRouter = this.getOwnerComponent().getRouter();
+
                     if (pressedTileId === apiSettingsTileId) {
-                        this.logLoadedData();
-
-                        // oRouter.navTo(routeApiSettingsTile, {}, true);
+                        this.getApiSettingsFromBackendAndRouteToApiSettingsView().then(r => oRouter.navTo(routeApiSettingsTile, {}, true));
                     } else {
-                        console.log("Routing to API Settings was not possible.");
+                        console.log(errorMessage);
                     }
-
-                    /*pressedTileId === apiSettingsTileId
-                        ? oRouter.navTo(routeApiSettingsTile, {}, true)
-                        : console.log("Routing to API Settings was not possible.");*/
                 },
-                logLoadedData: async function () {
-                    var oModel = new JSONModel();
-                    var url = "http://localhost:8080/config_backend/apisettings";
+                getApiSettingsFromBackendAndRouteToApiSettingsView: async () => {
                     $.get(url, function (response) {
                         console.log(response);
-                        oModel.setData(response);
                     });
-                },
+                }
             }
         );
     }
