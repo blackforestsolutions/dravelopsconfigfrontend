356d79990501f46c16204d0d2f79f9412f0d7edf
WillBaer <williambaer05@gmail.com>
2021-05-16 15:36:49 +0200

    Created muster form with all necessary features as in Documentation p. 4 in ApiSettingsView; Started with passing the Backend response to ApiSettingsView

--- webapp/controller/launchpad/configurations/ApiSettings.controller.js
+++ webapp/controller/launchpad/configurations/ApiSettings.controller.js
@@ -8,16 +8,20 @@ sap.ui.define([
     "use strict";
 
     var url = "http://localhost:8080/config_backend/apisettings";
+    var responseJson;
     return BaseController.extend("de.blackforestsolutions.dravelopsconfigfrontend.controller.launchpad.configurations.ApiSettings", {
         getApiSettingsFromBackend: async () => {
             $.get(url, function (response) {
-                console.log(response);// @will response ist das json
-
+                /**console.log(response);// @will response ist das json*/
+                responseJson = response;
             });
         },
 
         onInit: function () {
             this.getApiSettingsFromBackend();
+            var oModel = new JSONModel();
+            oModel.loadData("jsonfilename.json")
+            //JS Objekt in Json Model mappen
         }
     });
 });
